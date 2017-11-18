8f2c046968ec8c630e3915ace74f7d90c4fe301e
Nikolas Bogucarskis <niktechnopro@gmail.com>
2017-11-18 18:25:16 -0500

    with hash

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const mysql = require('mysql')
 const request_module = require('request');
 const bodyParser = require('body-parser');
 const router = express.Router();
+const bcrypt = require('bcrypt-nodejs');
+const swal = require('sweetalert');
 // const geocode = require('./geocode/geocode.js'); //module to extract lat,lng from zip code
 const port = process.env.PORT || 3000; //configures to available port based on
 //enviroment variable or port 3000 by default
@@ -64,7 +66,7 @@ app.get('/', (req, res) => {
 });
 
 //serves front login page and veryfies if user in database
-app.post('/loginForm', (req, res) => {
+app.post('/loginForm', (req, res, next) => {
     var email = req.body.email;
     var password = req.body.password;
     console.log("this is what received from form: ", email, password)
@@ -73,14 +75,20 @@ app.post('/loginForm', (req, res) => {
         //did this return a row? If so, the user already exists
         if (results.length != 0){
             console.log('users email is in database')
+            // res.send("User is in database")
+            res.render('chatBot',{
+             });
         }else{
             //this is a new user - insert them - user must register
             // const insertQuery = `INSERT INTO users (first_name, last_name, email, pw, child_name, relationship, child_username, fav_color, submission_date) VALUES (DEFAULT, ?,?,?);`;
             console.log('user must be inserted')
+            res.render('index', {
+                onLoad: 1
+            })
             
         }
     })
-    res.send('I got something here')
+    // res.send('I got something here')
 });
 
 //reading from registration form
@@ -95,71 +103,40 @@ app.post('/registerForm', (req, res)=>{
     var child_username = req.body.child_username;
     var favorite_color = req.body.favorite_color;
     var submission_date = req.body.submission_date;
+    var hash = bcrypt.hashSync(password);
+    console.log(hash);
     console.log(first_name, last_name, email, password, child_name, relationship, child_username, favorite_color, submission_date)
     const selectQuery = `SELECT * FROM parents WHERE email = ?;`;
     db.query(selectQuery, [email, last_name],(error, results)=>{
         //did this return a row? If so, the user already exists
         if (results.length != 0){
             console.log('user is in database, must login now')
+            res.render('index',{ });
         }else{
             //this is a new user - insert them - user must register
             console.log('user must be inserted')
             console.log('we have to render registration page for user to register')
             const insertQuery = `INSERT INTO parents (first_name, last_name, email, pw, child_name, relationship, child_username, fav_color, submission_date) VALUES (?,?,?,?,?,?,?,?,?);`;
-            db.query(insertQuery, [first_name, last_name, email, password, child_name, relationship, child_username, favorite_color, submission_date], (error)=>{
+            db.query(insertQuery, [first_name, last_name, email, hash, child_name, relationship, child_username, favorite_color, submission_date], (error)=>{
                 if (error){
                     console.log('error inserting into database')
                     throw error;
                 }else{
-                    console.log('succesful insertion into databse')
-                    res.send("we just updated database");
+                    console.log('succesful insertion into databse, next login')
+                    // res.send("we just updated database");
+                    swal("Good job!", "You clicked the button!", "success", {
+                      button: "Aww yiss!",
+                    });
+                    res.render('index',{
+                        onLoad: 2
+                    });
+                    
                 }
             })
         }
     })
 })
     
-// We need to make sure this email isn't already registered 
-//     const selectQuery = `SELECT * FROM users WHERE email = ?;`;
-//     connection.query(selectQuery,[email],(error,results)=>{
-//         // did this return a row? If so, the user already exists
-//         if(results.length != 0){
-//             res.redirect('/register?msg=registered');
-//         }else{
-//             // this is a new user. Insert them!
-//             // Hash the password first...
-//             var hash = bcrypt.hashSync(password);
-//             // var insertQuery = `INSERT INTO users VALUES (DEFAULT,?,?,?);`;
-//             var insertQuery = `INSERT INTO users (name, email, password) VALUES (?,?,?);`;
-//             connection.query(insertQuery,[name,email,hash],(error)=>{
-//                 if(error){
-//                     throw error;
-//                 }else{
-//                     res.redirect('/?msg=registered');
-//                 }
-//             });
-//         }
-//     });
-// });
-
-
-
-// app.get('/:uid', (request, response) => {// this is after clicking on link with doctor uid
-//     var uid = request.params.uid;
-//     console.log(uid);
-//     request_module({ url: `https://api.betterdoctor.com/2016-03-01/doctors/${uid}?user_key=6864ad983287baee8365ce0542f8c459`, json: true }, (error, res, drProfile) => {
-//         var parsedProfile = (drProfile);
-//         console.log(parsedProfile);
-//     });
-// });
-
-
-// app.post('/search', (request, response) => {
-//     var zip = request.body.zip_code;
-//     var per_page = request.body.per_page;
-//     console.log('got the data from form:', zip, per_page);
-//     response.redirect(`/results?zip=${zip}&per_page=${per_page}`); //render will let me render any of the templates withing view engine
-// })
 
 server.listen(port, (error) => {
     (error) ? console.log("your code sucks"): console.log(`listening on port ${port}`);
