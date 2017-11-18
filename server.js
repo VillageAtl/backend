4c0e6ebcc1067d3cda34652e8f725cfa139ec1c5
Nikolas Bogucarskis <niktechnopro@gmail.com>
2017-11-18 18:43:30 -0500

    commented out hash

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ app.post('/loginForm', (req, res, next) => {
     console.log("this is what received from form: ", email, password)
     const selectQuery = `SELECT * FROM parents WHERE email = ?;`;
     db.query(selectQuery, [email],(error, results)=>{
+        // var passwordsMatch = bcrypt.compareSync(password,results[0].pw)
         //did this return a row? If so, the user already exists
         if (results.length != 0){
             console.log('users email is in database')
@@ -117,7 +118,7 @@ app.post('/registerForm', (req, res)=>{
             console.log('user must be inserted')
             console.log('we have to render registration page for user to register')
             const insertQuery = `INSERT INTO parents (first_name, last_name, email, pw, child_name, relationship, child_username, fav_color, submission_date) VALUES (?,?,?,?,?,?,?,?,?);`;
-            db.query(insertQuery, [first_name, last_name, email, hash, child_name, relationship, child_username, favorite_color, submission_date], (error)=>{
+            db.query(insertQuery, [first_name, last_name, email, password, child_name, relationship, child_username, favorite_color, submission_date], (error)=>{
                 if (error){
                     console.log('error inserting into database')
                     throw error;
